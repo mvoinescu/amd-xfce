0806f0a96a2531d5df6194ecb91fe2531f5ff5a9
Erik Dubois <erik.dubois@gmail.com>
2017-11-20 09:10:33 +0100

    volumeicon added

--- 110-install-sound-v3.sh
+++ 110-install-sound-v3.sh
@@ -21,6 +21,7 @@ sudo pacman -S pavucontrol  --noconfirm --needed
 sudo pacman -S alsa-utils alsa-plugins alsa-lib alsa-firmware --noconfirm --needed
 sudo pacman -S gstreamer --noconfirm --needed
 sudo pacman -S gst-plugins-good gst-plugins-bad gst-plugins-base gst-plugins-ugly --noconfirm --needed
+sudo pacman -S volumeicon --noconfirm --needed
 
 echo "################################################################"
 echo "#########   sound software software installed   ################"
