6da94a64fdf76ab464e49b62488aabed11d9ff8d
Erik Dubois <erik.dubois@gmail.com>
2017-11-20 10:05:26 +0100

    autoenable bluetooth

--- 120-bluetooth-v2.sh
+++ 120-bluetooth-v2.sh
@@ -25,6 +25,8 @@ sudo pacman -S --noconfirm --needed blueberry
 sudo systemctl enable bluetooth.service
 sudo systemctl start bluetooth.service
 
+sudo sed -i 's/'#AutoEnable=false'/'AutoEnable=true'/g' /etc/bluetooth/main.conf
+
 echo "reboot your system then ..."
 echo "set with bluetooth icon in bottom right corner"
 echo "change to have a2dp if needed"
