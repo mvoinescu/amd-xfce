1ad4a1d2df5591f212d8b458f9d1fcf35e52cd9b
Erik Dubois <erik.dubois@gmail.com>
2017-12-08 19:40:45 +0100

    comparison

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -20,8 +20,7 @@ echo "#################################################"
 
 sudo pacman -Syyu
 sudo pacman -S lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings --noconfirm --needed
-sudo pacman -S xfce4 --noconfirm --needed
-sudo pacman -S xfce4-goodies --noconfirm --needed
+sudo pacman -S xfce4 xfce4-goodies --noconfirm --needed
 sudo systemctl enable lightdm.service -f
 sudo systemctl set-default graphical.target
 
