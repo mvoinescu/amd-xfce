86cf982d9b70ae043783ee1a694a841c050dd3c3
Erik Dubois <erik.dubois@gmail.com>
2018-01-23 08:49:20 +0100

    mpd ncmpcpp and polybar

--- 600-software-from-ArchMerge-repo-v1.sh
+++ 600-software-from-ArchMerge-repo-v1.sh
@@ -27,6 +27,7 @@ sudo pacman -S archmerge-iso-skel-git --noconfirm --needed
 #sudo pacman -S archmerge-openbox-themes-git --noconfirm --needed
 sudo pacman -S archmerge-pipemenus-git --noconfirm --needed
 sudo pacman -S archmerge-plank-themes-git --noconfirm --needed
+#sudo pacman -S archmerge-polybar-git --noconfirm --needed
 sudo pacman -S archmerge-root-git --noconfirm --needed
 sudo pacman -S archmerge-slimlock-themes-git --noconfirm --needed
 sudo pacman -S archmerge-termite-themes-git --noconfirm --needed
@@ -34,10 +35,34 @@ sudo pacman -S archmerge-termite-themes-git --noconfirm --needed
 sudo pacman -S archmerge-wallpapers-git --noconfirm --needed
 sudo pacman -S archmerge-xfce-thunar-git --noconfirm --needed
 
-echo "Copying all files and folders from /etc/skel to ~"
-
-cp -rT /etc/skel ~
 
 echo "################################################################"
 echo "####     Software from ArchMerge Repository installed       ####"
 echo "################################################################"
+echo
+
+
+echo "################################################################"
+echo "Copying all files and folders from /etc/skel to ~"
+echo "################################################################"
+echo
+cp -rT /etc/skel ~
+echo
+echo "################################################################"
+echo "removing all folders and files unnecessary for this dekstop from .config"
+echo "################################################################"
+echo
+rm ~/.config/autostart/calamares.desktop
+#rm -rf ~/.config/nitrogen
+rm -rf ~/.config/tint2
+rm -rf ~/.config/volumeicon
+rm ~/.config/compton.conf
+echo
+echo "################################################################"
+echo "removing all folders and files unnecessary for this desktop from .local"
+echo "################################################################"
+echo
+rm -rf ~/.local/share/applications
+#rm -rf ~/.local/share/xfpanel-switch
+
+
